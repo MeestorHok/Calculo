d144bf9ab4fa8117e074c650626a042484f04ddd
MeestorHok <mitchellja97@gmail.com>
2016-03-11 13:54:39 -0600

    prepared js for unit testing and linting

--- js/scripts.js
+++ js/scripts.js
@@ -1,173 +1,172 @@
 var evaluate = (function () {
-    function tokenize (str) {
-        return str.match(/\d+\.?\d*|[a-zA-Z]+|\S/g);
-    }
+  'use strict';
 
-    function substitute (rpn, value) {
-        while (rpn.indexOf('x') !== -1) {
-            rpn[rpn.indexOf('x')] = Number(value);
+  function tokenize(str) {
+    return str.match(/\d+\.?\d*|[a-zA-Z]+|\S/g);
+  }
+
+  function substitute(rpn, value) {
+    while (rpn.indexOf('x') !== -1) {
+      rpn[rpn.indexOf('x')] = Number(value);
+    }
+    return rpn;
+  }
+
+  function isNumeric(token) {
+    return !!(!isNaN(token) && isFinite(token));
+  }
+
+  function isLetter(token) {
+    return 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(token) !== -1;
+  }
+
+  function isOperator(token) {
+    return '^*/+-'.indexOf(token) !== -1;
+  }
+
+  function performOperation(a, b, operation) {
+    var ans = 0;
+    switch (operation) {
+    case '+':
+      ans = a + b;
+      break;
+    case '-':
+      ans = a - b;
+      break;
+    case '*':
+      ans = a * b;
+      break;
+    case '/':
+      ans = a / b;
+      break;
+    case '^':
+      ans = (function (a, b) {
+        var num = a;
+        while (b > 1) {
+          num *= a;
+          b -= 1;
         }
-        return rpn;
+        return num;
+      }(a, b));
+      break;
+    default:
+      break;
     }
+    return ans;
+  }
 
-    function isNumeric (token) {
-        return !!(!isNaN(token) && isFinite(token));
-    }
-
-    function isLetter (token) {
-        return 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(token) !== -1;
-    }
-
-    function isOperator (token) {
-        return '^*/+-'.indexOf(token) !== -1;
-    }
-
-    function performOperation (a, b, operation) {
-        var ans = 0;
-        switch (operation) {
-            case '+':
-                ans = a + b;
-                break;
-            case '-':
-                ans = a - b;
-                break;
-            case '*':
-                ans = a * b;
-                break;
-            case '/':
-                ans = a / b;
-                break;
-            case '^':
-                ans = (function (a, b) {
-                    var num = a;
-                    while (b > 1) {
-                        num *= a;
-                        b--;
-                    }
-                    return num;
-                })(a, b);
-                break;
-            default:
-                break;
+  function shuntingYard(infix) {
+    var stack = [],
+      queue = [],
+      i = 0,
+      token,
+      token2,
+      operators = {
+        '^': {
+          precedence: 4,
+          associativity: 'Right'
+        },
+        '/': {
+          precedence: 3,
+          associativity: 'Left'
+        },
+        '*': {
+          precedence: 3,
+          associativity: 'Left'
+        },
+        '+': {
+          precedence: 2,
+          associativity: 'Left'
+        },
+        '-': {
+          precedence: 2,
+          associativity: 'Left'
         }
-        return ans;
+      };
+    while (i < infix.length) {
+      token = infix[i];
+
+      if (isNumeric(token)) {
+        queue.push(Number(token));
+      } else if (isLetter(token)) {
+        queue.push(token);
+      } else if (isOperator(token)) {
+        token2 = stack[stack.length - 1];
+        while (isOperator(token2)
+                && (operators[token].associativity === 'Left'
+                && (operators[token].precedence <= operators[token2].precedence
+                || operators[token].associativity === 'Right')
+                && (operators[token].precedence < operators[token2].precedence))) {
+
+          queue.push(stack.pop());
+          token2 = stack[stack.length - 1];
+        }
+        stack.push(token);
+      } else if (token === '(') {
+        stack.push(token);
+      } else if (token === ')') {
+        while (stack[stack.length - 1] !== '(') {
+          queue.push(stack.pop());
+        }
+        stack.pop();
+      }
+
+      i += 1;
     }
 
-    function shuntingYard (infix) {
-        var stack = [],
-            queue = [],
-            i = 0,
-            operators = {
-                '^': {
-                    precedence: 4,
-                    associativity: 'Right'
-                },
-                '/': {
-                    precedence: 3,
-                    associativity: 'Left'
-                },
-                '*': {
-                    precedence: 3,
-                    associativity: 'Left'
-                },
-                '+': {
-                    precedence: 2,
-                    associativity: 'Left'
-                },
-                '-': {
-                    precedence: 2,
-                    associativity: 'Left'
-                }
-            };
-
-        while (i < infix.length) {
-            var token = infix[i];
-
-            if (isNumeric(token)){
-                queue.push(Number(token));
-            } else if(isLetter(token)) {
-                queue.push(token);
-            } else if (isOperator(token)) {
-                var token2 = stack[stack.length-1];
-                while (isOperator(token2)
-                && (operators[token].associativity == 'Left'
-                && operators[token].precedence <= operators[token2].precedence
-                || operators[token].associativity == 'Right'
-                && operators[token].precedence < operators[token2].precedence)) {
-
-                    queue.push(stack.pop());
-                    token2 = stack[stack.length-1];
-                }
-                stack.push(token);
-            } else if (token === '(') {
-                stack.push(token);
-            } else if (token === ')') {
-                while (stack[stack.length-1] != '(') {
-                    queue.push(stack.pop());
-                }
-                stack.pop();
-            }
-
-            i++;
-        }
-
-        while (stack.length > 0) {
-            queue.push(stack.pop());
-        }
-
-        return queue;
+    while (stack.length > 0) {
+      queue.push(stack.pop());
     }
 
-    function evaluate (equation) {
-        var a, b, stack = [];
+    return queue;
+  }
 
-        while (equation.length > 0) {
-            if (isNumeric(equation[0])) {
-                stack.push(equation.shift());
-            } else if (isOperator(equation[0])) {
-                b = stack.pop();
-                a = stack.pop();
-                stack.push(performOperation(a, b, equation.shift()));
-            }
-        }
+  function evaluate(equation) {
+    var a, b, stack = [];
 
-        return stack[0];
+    while (equation.length > 0) {
+      if (isNumeric(equation[0])) {
+        stack.push(equation.shift());
+      } else if (isOperator(equation[0])) {
+        b = stack.pop();
+        a = stack.pop();
+        stack.push(performOperation(a, b, equation.shift()));
+      }
     }
 
-    return function (expression, value) {
-        value = value || 0;
-        var infix = tokenize(expression),
-            rpn = substitute(shuntingYard(infix), value);
+    return stack[0];
+  }
 
-        return evaluate(rpn);
-    };
-})();
+  return function (expression, value) {
+    value = value || 0;
+    var infix = tokenize(expression),
+      rpn = substitute(shuntingYard(infix), value);
 
-function f (func, x) {
+    return evaluate(rpn);
+  };
+}());
+
+var calculus = (function () {
+  "use strict";
+  var self = this;
+
+  self.f = function (func, x) {
     return evaluate(func, x);
-}
+  };
 
-function df (func, x) {
+  self.df = function (func, x) {
     var h = 0.0001;
-    return Math.round((f(func, x + h) - f(func, x))/h);
-}
+    return Math.round((self.f(func, x + h) - self.f(func, x)) / h);
+  };
 
-function newtonsMethod (func, guess) {
+  self.newtonsMethod = function (func, guess) {
     var tmp = 0;
     while (Math.abs(guess - tmp) > 0.00001) {
-        tmp = guess;
-        guess = guess - (f(func, guess)/df(func, guess));
+      tmp = guess;
+      guess = guess - (self.f(func, guess) / self.df(func, guess));
     }
     return guess;
-}
+  };
 
-
-var func = 'x^2-9';
-var num = 6;
-
-console.log(
-    'X=' + num +
-    '; f(x)=' + f(func, num) +
-    '; df/dx(x)=' + df(func, num));
-
-console.log('Newtons Method: guess=' + 5 + '; expect=' + 3 + '; real=' + newtonsMethod(func, 3));
+  return self;
+}());
